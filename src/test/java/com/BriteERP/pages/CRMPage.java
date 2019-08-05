e91646c30f3dc409755b0460a86eff96f00442fa
Eyyup Celik <cider31@gmail.com>
2019-08-05 10:30:53 +0200

    Update CRMPage.java
    CRM Page locators added for edit quotation test

--- src/test/java/com/BriteERP/pages/CRMPage.java
+++ src/test/java/com/BriteERP/pages/CRMPage.java
@@ -25,6 +25,52 @@ public class CRMPage {
     @FindBy(xpath = "//button[@class='btn btn-sm btn-primary']")
     public WebElement okButtonAfterDelete;
 
+    	@FindBy(xpath = "//*[@accesskey='c']")
+	public WebElement createButton;
+
+	@FindBy(css = "[data-original-title= 'Kanban']")
+	public WebElement kanbanView;
+
+	@FindBy(css = "[data-original-title= 'List']")
+	public WebElement listView;
+
+	@FindBy(id = "#o_field_input_62")
+	public WebElement OpportunityTitleInput;
+	
+	@FindBy(id = "#o_field_input_63")
+	public WebElement customerInput;
+	
+	@FindBy(id = "#o_field_input_64")
+	public WebElement revenueInput;
+	
+	@FindBy(xpath = "//*[text()='Create a Customer']")
+	public WebElement createACustomerTitle;
+	
+	@FindBy(xpath = "//*[text()='Create a Customer']/parent::div/following::div[2]/button[1]/span")
+	public WebElement createCustomerSubMenu_CreateButton;
+	
+	@FindBy(xpath = "//*[@data-menu='274']/span")
+	public WebElement sideMenu_Pipeline;
+	
+	@FindBy(css = ".o_required_modifier[placeholder='e.g. Product Pricing']")
+	public WebElement opportunityNameInput;
+	
+	@FindBy(css = ".o_field_widget>input[class='o_input']")
+	public WebElement opportunityExpectedRevenueInput;
+	
+	@FindBy(css = ".o_form_buttons_edit>button")
+	public WebElement pipelineSaveButton;
+	
+	@FindBy(css = ".o_form_button_edit")
+	public WebElement pipelinEditButton;
+	
+	public WebElement getPipelineFromKanbanList(int rowNumber) {
+		String locator = "[data-id='1']>div:nth-child("+ (rowNumber+1)+ ")";
+		System.out.println("locator = " + locator);
+		WebElement pipeline = Driver.getDriver().findElement(By.cssSelector(locator));
+		return pipeline;
+	}
+
 
     public void clickQuotationNumber(String... clickQuotNumber){
 
